4a5a401daee020410c959cd72e4836fcec27dfa4
Nazila Faizzadah <nazilafaizzadah37@gmail.com>
2025-05-20 07:05:53 +0430

    C & R & data.js file added

---
+++ controllers/postController.js
@@ -0,0 +1,18 @@
+
+export const getAllposts = (req,res) =>{
+
+};
+
+export const getpost = (req,res) =>{
+    
+};
+
+export const updatePost = (req,res) =>{
+    
+};
+
+export const deletePost = (req,res) =>{
+    
+};
+
+
--- index.js
+++ index.js
@@ -2,23 +2,13 @@
 import express from 'express';
 import methodOverride from "method-override";
 import multer from 'multer';
+import data from './data.js';
 // making the application
 const app=express();
 // useing port 3000
-const port=3000;
+const port=3001;
 // used this middleware for our static files
 app.use(express.static("public"));
-// my data
-let data=[
-    {id:1, name:"Front-End Development",description:" I got this certificate from CTI(Code To Inspire) for completing 15 months web development.",images:"/images/font-end.jpg"},
-    {id:2, name:"Web Development",description:"I got this certificate from USC(University of Southern California) for comleting introduction to web development course.",images:"/images/html.jpg"},
-    {id:3, name:"English",description:"I got this certificate from JRS(Jesuit WorldWide Learining) for completing intermediate level.",images:"/images/intermedaite.jpg"},
-    {id:4, name:"Python",description:"I got this certificate from  USC(University of Southern California) for completing introduction to python programming language.",images:"/images/python.jpg"},
-    {id:5, name:"Soft Skills",description:"I got this certificate from W4Girls it was an e-Mentoring programme for eight weeks.",images:"/images/w4girls.jpg"},
-    {id:6, name:"JavaScript",description:"I got this certificate from USC(University of Southern California) for completing the JavaScript Fundamentals course.",images:"/images/JavaScript.jpg"},
-    {id:7, name:"English",description:"I got this certificate from JRS(Jesuit WorldWide Learining) for completing beginner level.",images:"/images/bigner.jpg"}
-   
-]
 // I made this variable because of making the id for new posts
 let newId=data.length+1;
 
