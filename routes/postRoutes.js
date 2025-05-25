01a37afbfba47ba792797296335dd1d0db9dd57c
Nazila Faizzadah <nazilafaizzadah37@gmail.com>
2025-05-25 15:12:46 +0430

    post middleware added

--- routes/postRoutes.js
+++ routes/postRoutes.js
@@ -7,21 +7,9 @@ import {
   getpost,
   getPostpage,
 } from "../controllers/postController.js";
-import multer from "multer";
-
+import upload from "../middlewares/postMiddleware.js";
 const router = express.Router();
 
-const storage = multer.diskStorage({
-  // here I determine where my images should be save
-  destination: function (req, file, cb) {
-    cb(null, "public/images");
-  },
-  // and here determine the name of my images
-  filename: function (req, file, cb) {
-    cb(null, file.originalname);
-  },
-});
-const upload = multer({ storage: storage });
 router.get("/", getAllposts);
 router.get("/new_post", getPostpage);
 router.get("/edit/:id", getpost);
